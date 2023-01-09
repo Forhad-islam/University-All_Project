d799b806166b77b2dbe2b2982476a0e317cc206a
MD. FORHADUL ISLAM <103302239+Forhad-islam@users.noreply.github.com>
2023-01-09 12:08:48 +0600

    Delete JAVA Project directory

--- JAVA Project/teacherStudent/A.java
+++
@@ -1,8 +0,0 @@
-package teacherStudent;    // declearing package name
-public interface A{        // A class declear as interface class
-    double MidTerm() ;    // abstract function.
-    }
-
-interface B {             // B class declear as interface class
-    double FinalTerm();    // abstract function.
-}
